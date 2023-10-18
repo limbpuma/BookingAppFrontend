183b7addd7724b2cea222cd9a8183ff912c55cd0
Limber <limbpuma_de@hotmail.com>
2023-10-18 12:01:24 +0200

    changing urlBase

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -7,7 +7,7 @@ import { Booking } from './booking';
   providedIn: 'root'
 })
 export class BookingService {
-  private urlBase = 'https://bookingappbackend-production.up.railway.app/'; 
+  private urlBase = 'bookingappbackend-production.up.railway.app'; 
 
   constructor(private clienteHttp: HttpClient) { }
 
