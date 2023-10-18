77bbd8a1e36dc87015b5e9b08ec399911aa7e46f
Limber <limbpuma_de@hotmail.com>
2023-10-18 18:18:35 +0200

    booking-servie update url added

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -6,31 +6,31 @@ import { Booking } from './booking';
 @Injectable({
   providedIn: 'root'
 })
-export class BookingService {
-  private urlBase = 'https://bookingappbackend-production.up.railway.app'; 
+export class BookingService { 
+  private urlBase = 'https://booking-backend-production-c3a1.up.railway.app'; 
 
   constructor(private clienteHttp: HttpClient) { }
 
   getBookingList(): Observable<Booking[]> {
-    return this.clienteHttp.get<Booking[]>(this.urlBase);
+    return this.clienteHttp.get<Booking[]>(`${this.urlBase}/restaurant-app/booking`);
   }
  
   addBooking(booking: Booking): Observable<Object> {
-    return this.clienteHttp.post(this.urlBase, booking);
+    return this.clienteHttp.post(`${this.urlBase}/restaurant-app/booking`, booking);
   }
 
   editBooking(id: number, booking: Booking): Observable<Object> {
-    return this.clienteHttp.put(`${this.urlBase}/${id}`, booking);
+    return this.clienteHttp.put(`${this.urlBase}/restaurant-app/booking/${id}`, booking);
   }
  
 
    getBookingById(id: number): Observable<Booking> {
-    return this.clienteHttp.get<Booking>(`${this.urlBase}/${id}`);
+    return this.clienteHttp.get<Booking>(`${this.urlBase}/restaurant-app/booking/${id}`);
   }
 
   deactivateBooking(id: number, booking: Booking): Observable<Object> {
     booking.active = false;
-    return this.clienteHttp.put(`${this.urlBase}/deactivate/${id}`, booking);
+    return this.clienteHttp.put(`${this.urlBase}/restaurant-app/booking/deactivate/${id}`, booking);
 }
 
 
